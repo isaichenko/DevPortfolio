f4b3d809e97d619d4849868d5867cb715eb9444c
isaichenko <isaichenko.e@gmail.com>
2018-03-01 13:07:33 +0200

    Built out initial form and partial call for comments along with controller actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
   class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_blog, only: [:edit, :update, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index],
          user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]},
@@ -15,6 +15,9 @@
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
@@ -69,7 +72,7 @@
     elsif @blog.published?
       @blog.draft!
     end
-        
+
     redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
