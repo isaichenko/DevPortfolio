c0a2765836714e6a0f79f97367fa988325d2a712
isaichenko <isaichenko.e@gmail.com>
2018-03-03 16:55:26 +0200

    Update gemfile and declared ruby version and updated some layouts styles

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Jordan Hudgens portfolio"
+    @page_title = "Eugene Isaichenko | My Portfolio Website"
+    @seo_keywords = "Eugene Isaichenko portfolio"
   end
 end
